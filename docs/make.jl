f15075c0c334c0fa6a0b6a295e6b8f528db7b55b
Tamas K. Papp <tkpapp@gmail.com>
2018-03-10 09:53:43 +0100

    fix repo name

--- docs/make.jl
+++ docs/make.jl
@@ -3,4 +3,4 @@ using Documenter, GitlabJuliaDemo
 makedocs(format = :html,
          sitename = "GitlabJuliaDemo",
          pages = ["index.md"],
-         repo = "https://gitlab.com/tkpapp/GitlabJuliaDemo/blob/{commit}{path}#{line}")
+         repo = "https://gitlab.com/tkpapp/GitlabJuliaDemo.jl/blob/{commit}{path}#{line}")
