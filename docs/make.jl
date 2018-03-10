4ea8fc963d0b7a7ae8237849eaa3d0e0f8eba077
Tamas K. Papp <tkpapp@gmail.com>
2018-03-10 09:39:17 +0100

    Fix gitlab source reference.
    Cf https://github.com/JuliaDocs/Documenter.jl/pull/662
    https://github.com/JuliaDocs/Documenter.jl/issues/661

--- docs/make.jl
+++ docs/make.jl
@@ -3,4 +3,4 @@ using Documenter, GitlabJuliaDemo
 makedocs(format = :html,
          sitename = "GitlabJuliaDemo",
          pages = ["index.md"],
-         repo = "https://gitlab.com/tkpapp/GitlabJuliaDemo/blob/{commit}{path}#L{line}")
+         repo = "https://gitlab.com/tkpapp/GitlabJuliaDemo/blob/{commit}{path}#{line}")
